a350e9228ad2d4688eab0c845905eb527bdd807d
timpettenkofer <65283420+timpettenkofer@users.noreply.github.com>
2022-01-19 10:41:55 +0100

    Add files via upload

---
+++ Sourcecode/HoPoSim3D/Assets/ConcaveHullGenerator/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace ConcaveHull
+{
+	public class Node
+	{
+		public int id;
+		public double x;
+		public double y;
+		public double cos; // Used for middlepoint calculations
+		public Node(double x, double y, int id)
+		{
+			this.x = x;
+			this.y = y;
+			this.id = id;
+		}
+
+		public Vector2 ToVector2()
+		{
+			return new Vector2(Convert.ToSingle(x), Convert.ToSingle(y));
+		}
+	}
+}
